17e300e551af3ba8fb76325f609d29c0ddb0da62
liuzx <3293344726@qq.com>
2023-03-16 15:38:55 +0800

    hot-fix commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("11111");
+        System.out.println("find a mistake");
     }
 }
